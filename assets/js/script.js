d1b61dce3ec855f8225f1b90afc70616725078f4
Anuraj <anurajtr.stratagile@gmail.com>
2023-02-14 12:31:45 +0530

    happenig

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$('.spo-happenings__tile-wrap').slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
