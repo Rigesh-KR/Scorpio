4b3a84ea8da8a633fb481c462e2cf7053235dec4
rigeshKR <rigeshkumar@gmail.com>
2023-02-15 10:57:57 +0530

    Updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
+//   Navbar  Start
+function openNav() {
+  // document.getElementById("mySidenav").style.width = "250px";
+  var slide = "right:0; top:0; width:300px;";
+  document.getElementById("Sidenav").style = slide;
+   document.getElementById("CanvasNav").style.width = "100%";
+    document.getElementById("CanvasNav").style.opacity = "1";  
+}
+function closeNav() {
+  document.getElementById("Sidenav").style.right = "-300px";
+  document.getElementById("CanvasNav").style.width = "0%";
+  document.getElementById("CanvasNav").style.opacity = "0"; 
+}
+function openOffcanvas() {
+  document.getElementById("CanvasNav").style.opacity = "1"; 
+}
+function closeOffcanvas() {
+  document.getElementById("Sidenav").style.right = "-300px";
+  document.getElementById("CanvasNav").style.opacity = "0"; 
+  document.getElementById("CanvasNav").style.width = "0"; 
+}
+//   Navbar  End
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() > 60) {
+    $(".spo-header").addClass("sticky");
+  } else {
+    $(".spo-header").removeClass("sticky");
+   // $("body").removeClass("overlay");
+  }
+});
 //   Happenig module start
 $(".spo-happenings__tile-wrap").slick({
   dots: true,
