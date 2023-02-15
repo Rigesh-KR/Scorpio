0469820845eeb5afc3c3f605671f30725f5cb52f
rigeshKR <rigeshkumar@gmail.com>
2023-02-15 15:39:00 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,75 +29,54 @@ $(window).on("scroll", function () {
   }
 });
 //   Happenig module start
-
-
-const slider = $(".spo-happenings__tile-wrap");
-const slider_tiles = document.querySelectorAll(".spo-news-tile");
-slider
-  .slick({
+$(".spo-happenings__tile-wrap").slick({
   dots: true,
+  infinite: false,
+  // speed: 500,
   slidesToShow: 3,
+  slidesToScroll: 1,
   prevArrow: null,
   nextArrow: null,
-  });
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
 
-slider.on('wheel', (function(e) {
+// to prevent the scroll of the page until all tiles shown starts
+const slider = $(".spo-happenings__tile-wrap");
+const slider_tiles = document.querySelectorAll(".spo-news-tile");
+
+slider.on("wheel", function (e) {
   e.preventDefault();
 
-  if (e.originalEvent.deltaY < 0) {
-    $(this).slick('slickNext');
+  if (e.originalEvent.deltaY < slider_tiles.length - 1) {
+    !e.preventDefault();
   } else {
-    $(this).slick('slickPrev');
+    $(this).slick("slickNext");
   }
-}));
-// ANURAJ============
-// $(".spo-happenings__tile-wrap2").slick({
-//   dots: true,
-//   infinite: false,
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   prevArrow: null,
-//   nextArrow: null,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         dots: true,
-//       },
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 1,
-//       },
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//       },
-//     },
-//   ],
-// });
-// ANURAJ
-// to prevent the scroll of the page until all tiles shown starts
-// const slider = $(".spo-happenings__tile-wrap");
-// const slider_tiles = document.querySelectorAll(".spo-news-tile");
-
-// slider.on("wheel", function (e) {
-//   e.preventDefault();
-
-//   if (e.originalEvent.deltaY < slider_tiles.length - 1) {
-//     !e.preventDefault();
-//   } else {
-//     $(this).slick("slickNext");
-//   }
-// });
+});
 // to prevent the scroll of the page until all tiles shown ends
 //  Happenig module end
 //   Shop module start
