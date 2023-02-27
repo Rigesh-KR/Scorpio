8d325de0d566919225e36f396fa3f47722db7fdb
rigeshKR <rigeshkumar@gmail.com>
2023-02-28 01:55:43 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -287,3 +287,104 @@ $('#datepicker').datepicker({
 });
 
 // clientWidth
+
+// -----------------
+const paginationNumbers = document.getElementById("pagination-numbers");
+const paginatedList = document.getElementById("paginated-list");
+const listItems = paginatedList.querySelectorAll("li");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+const paginationLimit = 10;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+const disableButton = (li) => {
+  li.classList.add("disabled");
+  li.setAttribute("disabled", true);
+};
+
+const enableButton = (li) => {
+  li.classList.remove("disabled");
+  li.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination-number, .spo-pagn__link").forEach((li) => {
+    li.classList.remove("active");
+    const pageIndex = Number(li.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      li.classList.add("active");
+    }
+  });
+};
+
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement("li");
+  pageNumber.className = "spo-pagn__link";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + index);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+  handlePageButtonsStatus();
+  
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {
+    item.classList.add("hiddenPag");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hiddenPag");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
+
+  document.querySelectorAll(".pagination-number, .spo-pagn__link").forEach((li) => {
+    const pageIndex = Number(li.getAttribute("page-index"));
+
+    if (pageIndex) {
+      li.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
