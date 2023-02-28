5094e32e1b61f2371016a0b6b93df6da7ffb5b90
Sanjay <sanjay.e@strat-agile.com>
2023-02-28 15:02:05 +0530

    footer-update

--- assets/js/script.js
+++ assets/js/script.js
@@ -287,3 +287,34 @@ $('#datepicker').datepicker({
 });
 
 // clientWidth
+const accordionBody = document.querySelectorAll('.accordion-collapse');
+const accordionBtn = document.querySelectorAll('.accordion-button');
+// on resize
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 768) {
+    accordionBody.forEach((e, ele) => {
+      e.classList.remove('show');
+      accordionBtn[ele].ariaExpanded = 'false';
+    })
+  } else {
+    accordionBody.forEach((e, ele) => {
+      e.classList.add('show');
+      accordionBtn[ele].ariaExpanded = 'true';
+    })
+  }
+})
+// on load
+window.addEventListener('load', () => {
+  if (window.innerWidth <= 768) {
+    accordionBody.forEach((e, ele) => {
+      e.classList.remove('show');
+      accordionBtn[ele].ariaExpanded = 'false';
+    })
+  } else {
+    accordionBody.forEach((e, ele) => {
+      e.classList.add('show');
+      accordionBtn[ele].ariaExpanded = 'true';
+    })
+  }
+})
+
